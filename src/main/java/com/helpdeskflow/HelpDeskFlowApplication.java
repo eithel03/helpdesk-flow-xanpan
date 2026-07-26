89505d46b7c7c8c456d27a4730849ab54493f6f3
eithel03 <eithel.herrera03@gmail.com>
2026-07-25 23:25:11 -0600

    config: crear proyecto base con Java 17 y Maven

---
+++ src/main/java/com/helpdeskflow/HelpDeskFlowApplication.java
@@ -0,0 +1,8 @@
+package com.helpdeskflow;
+
+public class HelpDeskFlowApplication {
+
+    public static void main(String[] args) {
+        System.out.println("HelpDesk Flow iniciado correctamente.");
+    }
+}
